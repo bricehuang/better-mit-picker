36326d6046856d861544026ddc131b09f3aea333
Brice Huang <bmhuang@mit.edu>
2016-09-17 20:19:17 -0400

    lol

--- exampleApp.py
+++ exampleApp.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/elmo/')
+def elmo_meeting():
+    return 'ELMO MEETING! ELMO MEETING! THIS CODE IS VERY RELEVANT!'
